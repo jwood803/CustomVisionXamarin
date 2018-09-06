cbb6d9c64bff878391defbc9eeea9efa7fb7812b
J W <woodjon@gmail.com>
2018-09-06 09:18:56 -0400

    Initial add

---
+++ HerbsCustomVision/HerbsCustomVision/CameraPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HerbsCustomVision
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CameraPage : ContentPage
+	{
+        CameraViewModel _cameraViewModel;
+
+		public CameraPage ()
+		{
+			InitializeComponent ();
+
+            _cameraViewModel = new CameraViewModel();
+
+            BindingContext = _cameraViewModel;
+		}
+	}
+}
---
+++ HerbsCustomVision/HerbsCustomVision/CameraViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HerbsCustomVision
+{
+    public class CameraViewModel
+    {
+        public Command AccessCamera { get; set; }
+
+        public CameraViewModel()
+        {
+            AccessCamera = new Command(GetCameraAccess);
+        }
+
+        void GetCameraAccess()
+        {
+            if (CrossMedia.Current.Initialize().Result && CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
+            {
+                var mediaOptions = new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true
+                };
+
+                var file = CrossMedia.Current.TakePhotoAsync(mediaOptions).Result;
+            }
+        }
+    }
+}
